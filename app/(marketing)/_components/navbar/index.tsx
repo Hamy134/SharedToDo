3b839edef8ab2490a2f7f8ad91c2c5afe4017183
Korab Imeri <korabimeri91@gmail.com>
2024-06-11 18:35:30 +0200

    style: added small updates to the hero section

--- app/(marketing)/_components/navbar/index.tsx
+++ app/(marketing)/_components/navbar/index.tsx
@@ -7,7 +7,7 @@ export const Navbar = async () => {
   const isSubscribed = await checkSubscription();
 
   return (
-    <header className="flex items-center justify-between px-10 border-b shadow-md fixed w-full py-1 bg-background">
+    <header className="flex items-center justify-between px-10 border-b border-slate-300/30 fixed w-full py-1 bg-white/20 backdrop-blur-md z-50">
       <Logo />
 
       <div className="flex items-center justify-between">
